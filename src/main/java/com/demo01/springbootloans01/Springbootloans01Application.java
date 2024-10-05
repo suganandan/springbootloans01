27889bec9be76eca9d97f2789e8cf97a4df2691f
suganandan <suganandan@hotmail.com>
2024-10-05 19:35:24 +0530

    New Commit

---
+++ src/main/java/com/demo01/springbootloans01/Springbootloans01Application.java
@@ -0,0 +1,13 @@
+package com.demo01.springbootloans01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootloans01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootloans01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo01/springbootloans01/Springbootloans01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo01.springbootloans01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootloans01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
